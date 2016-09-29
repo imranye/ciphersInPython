2d629e1f85c7d27bfb70ddfec2bf7ebff63bbe11
Imran Muthuvappa <imuthuvappa@gmail.com>
2016-09-29 13:22:19 -0400

    Beginning to document my code so its easier for others to use it!

--- encryptingtransposition.py
+++ encryptingtransposition.py
@@ -1,7 +1,8 @@
-import math
-import random
+import math #used for pythons math functions
+import random #python random library
 
 def encrypt(key, message):
+    #takes in a key and a message, only encrypts
     ciphertext = [''] * key
     for x in range(key):
         pointer = x
@@ -13,6 +14,7 @@ def encrypt(key, message):
 
 
 def main():
+    #takes user input, prints encrypted result
     message = raw_input('enter a message: ')
     key = int(raw_input('select a key'))
     print(encrypt(key, message))
